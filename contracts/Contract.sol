3bd8c8707b9e1e7c8fefd4df194b176d7763bd14
Foshati <amirrezafoshati@gmail.com>
2025-01-28 23:49:42 +0330

    Add BuyMeACoffee contract with functionality to purchase coffee and track total sales

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,27 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract BuyMeACoffee {
+    struct coffee {
+        address sender;
+        string message;
+        uint256 timestamp;
+    }
+    uint256 totalCoffees;
+    address public owner;
+
+    event NewCoffee(address indexed sender, string message, uint256 timestamp);
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+    function buyMeACoffee(string memory _message) public payable {
+        require(msg.value >= 0, "value must be greater than 0");
+        totalCoffees += 1;
+        payable(owner).transfer(msg.value);
+        emit NewCoffee(msg.sender, _message, block.timestamp);
+    }
+    function getTotalCoffees() public view returns (uint256) {
+        return totalCoffees;
+    }
 }
